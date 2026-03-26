53faca14d9a27b50528c3ea1a9492c69781e73aa
shreyasingh965 <shreya.singh9@outlook.com>
2026-03-26 19:02:28 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(({ 
+  base: '/portfolio-shreyasingh-c9955c5c/', 
+}) => ({
   server: {
     host: "::",
     port: 8080,
