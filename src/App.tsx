d60ebf8cbae8ea465f92ab94922e9ef0bbc63643
shreyasingh965 <shreya.singh9@outlook.com>
2026-03-26 19:55:46 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/portfolio-shreyasingh-c9955c5c/">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
